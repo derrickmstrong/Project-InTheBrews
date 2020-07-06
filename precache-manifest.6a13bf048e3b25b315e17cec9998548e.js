1ecae8c94ca8ab0020e0171603ac38859fd6b54b
Derrick Strong <derrickmstrong@gmail.com>
2020-07-06 13:48:37 -0500

    Updates

---
+++ precache-manifest.6a13bf048e3b25b315e17cec9998548e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ea1c4b642e9a4c430b0ee6e5ff61f7",
+    "url": "/project-inthebrew/index.html"
+  },
+  {
+    "revision": "8a3bca0779544cd59c0b",
+    "url": "/project-inthebrew/static/css/main.6556fe50.chunk.css"
+  },
+  {
+    "revision": "29773283d3d62ef1a63b",
+    "url": "/project-inthebrew/static/js/2.73d3000a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-inthebrew/static/js/2.73d3000a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a3bca0779544cd59c0b",
+    "url": "/project-inthebrew/static/js/main.cdca306c.chunk.js"
+  },
+  {
+    "revision": "d3b3886e8757da307646",
+    "url": "/project-inthebrew/static/js/runtime-main.bf42e765.js"
+  }
+]);
